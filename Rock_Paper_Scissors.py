c6a0700a6822c9ba4ec0650345779b36859e1576
Armin.msv <arminmsv01@gmail.com>
2026-06-08 18:57:55 +0100

    Create Rock_Paper_Scissors.py

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,32 @@
+print("Type 'Exit' to quit the game anytime.")
+import random
+choices = ["Rock" , "Paper" , "Scissor"]
+win = ""
+while True :
+    computer = random.choice(choices)
+    user = input("Rock , Paper , Scissor: ").capitalize()
+
+    if user == "Exit":
+        break
+
+    if user not in choices:
+        print("Invalid choice!")
+    else:
+
+        if user == "Rock" and computer == "Scissor":
+            win = "Congratulations, you won!"
+
+        elif user == "Paper" and computer == "Rock":
+            win = "Congratulations, you won!"
+
+        elif user == "Scissor" and computer == "Paper":
+            win = "Congratulations, you won!"
+
+        elif user == computer:
+            win = "Equal!"
+
+        else:
+            win = "You lost, try again!"
+
+        print("Computer: " , computer)
+        print(win) 
