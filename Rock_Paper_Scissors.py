083b70b1ba1c386b30bb00eb0c5820c597b5ac9c
Armin.msv <arminmsv01@gmail.com>
2026-06-09 13:05:13 +0100

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -2,19 +2,20 @@ import random
 print("Type 'Exit' to quit the game anytime.")
 choices = ["Rock" , "Paper" , "Scissor"]
 win = ""
-you_win = 0
+wins = 0
 losses = 0
 draws = 0
-point = 0
+points = 0
 while True :
     computer = random.choice(choices)
     user = input("Rock , Paper , Scissor: ").strip().capitalize()
 
     if user == "Exit":
-        print("Wins: " , you_win)
-        print("Draws: " , draws)
-        print("losses: " , losses)
-        print("your point: " , point)
+        print(" Game Statistics")
+        print("Wins:", wins)
+        print("Losses:", losses)
+        print("Draws:", draws)
+        print("Points:", points)
         break
 
     if user not in choices:
@@ -23,23 +24,23 @@ while True :
 
         if user == "Rock" and computer == "Scissor":
             win = "Congratulations, you won!"
-            you_win += 1
-            point += 3
+            wins += 1
+            points += 3
 
         elif user == "Paper" and computer == "Rock":
             win = "Congratulations, you won!"
-            you_win += 1
-            point += 3
+            wins += 1
+            points += 3
 
         elif user == "Scissor" and computer == "Paper":
             win = "Congratulations, you won!"
-            you_win += 1
-            point += 3
+            wins += 1
+            points += 3
 
         elif user == computer:
             win = "Equal!"
             draws += 1
-            point += 1
+            points += 1
 
         else:
             win = "You lost, try again!"
